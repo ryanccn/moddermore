b567175bdaa5c558e86f26cdb03a5e828ddc4043
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-07-19 13:40:02 +0800

    curseforge type tweaks
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- lib/curseforge.ts
+++ lib/curseforge.ts
@@ -1,4 +1,4 @@
-import type { RichMod } from "./extra.types";
+import type { RichMod } from './extra.types';
 
 // type Side = 'required' | 'optional' | 'unsupported';
 
@@ -21,21 +21,19 @@ interface NetworkResult {
   isFeatured: boolean;
   primaryCategoryId: number;
 
-  categories: [
-    {
-      id: number;
-      gameId: number;
-      name: string;
-      slug: string;
-      url: string;
-      iconUrl: string;
-      dateModified: string;
-      isClass: boolean;
-      classId: number;
-      parentCategoryId: number;
-      displayIndex: number;
-    }
-  ];
+  categories: {
+    id: number;
+    gameId: number;
+    name: string;
+    slug: string;
+    url: string;
+    iconUrl: string;
+    dateModified: string;
+    isClass: boolean;
+    classId: number;
+    parentCategoryId: number;
+    displayIndex: number;
+  }[];
 
   classId: number;
   authors: {
@@ -85,15 +83,13 @@ interface NetworkResult {
     downloadUrl: string;
     gameVersions: string[];
 
-    sortableGameVersions: [
-      {
-        gameVersionName: string;
-        gameVersionPadded: string;
-        gameVersion: string;
-        gameVersionReleaseDate: string;
-        gameVersionTypeId: number;
-      }
-    ];
+    sortableGameVersions: {
+      gameVersionName: string;
+      gameVersionPadded: string;
+      gameVersion: string;
+      gameVersionReleaseDate: string;
+      gameVersionTypeId: number;
+    }[];
 
     dependencies: {
       modId: number;
@@ -106,24 +102,20 @@ interface NetworkResult {
     isServerPack: boolean;
     serverPackFileId: number;
     fileFingerprint: number;
-    modules: [
-      {
-        name: string;
-        fingerprint: number;
-      }
-    ];
+    modules: {
+      name: string;
+      fingerprint: number;
+    }[];
   }[];
 
-  latestFilesIndexes: [
-    {
-      gameVersion: string;
-      fileId: number;
-      filename: string;
-      releaseType: 1;
-      gameVersionTypeId: number;
-      modLoader: number;
-    }
-  ];
+  latestFilesIndexes: {
+    gameVersion: string;
+    fileId: number;
+    filename: string;
+    releaseType: 1;
+    gameVersionTypeId: number;
+    modLoader: number;
+  }[];
 
   dateCreated: string;
   dateModified: string;
@@ -139,10 +131,10 @@ export const getInfo = async (id: string): Promise<RichMod | null> => {
   // console.log(process.env);
 
   const API_KEY = process.env.CURSEFORGE_API_KEY;
-  if (!API_KEY) throw new Error("No CURSEFORGE_API_KEY defined!");
+  if (!API_KEY) throw new Error('No CURSEFORGE_API_KEY defined!');
 
   const res = await fetch(`https://api.curseforge.com/v1/mods/${id}`, {
-    headers: { "x-api-key": API_KEY },
+    headers: { 'x-api-key': API_KEY },
   });
 
   if (res.status === 404) {
@@ -155,7 +147,7 @@ export const getInfo = async (id: string): Promise<RichMod | null> => {
     id,
     href: `https://curseforge.com/minecraft/mc-mods/${data.slug}`,
     iconUrl: data.logo.thumbnailUrl ?? undefined,
-    provider: "curseforge",
+    provider: 'curseforge',
     name: data.name,
     description: data.summary,
   };
