77b44f10b356269f11587bc043a3e9da57b74549
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-07-28 19:42:09 +0800

    fix: better funny proxy
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- pages/api/cursed.ts
+++ pages/api/cursed.ts
@@ -12,14 +12,19 @@ const h = async (req: Request) => {
     });
   }
 
-  if (!new URL(originalURL).hostname.includes('forgecdn.net')) {
+  if (!new URL(originalURL).hostname.endsWith('forgecdn.net')) {
     return new Response('only forgecdn.net links work', {
       status: 400,
       headers: { 'content-type': 'text/plain' },
     });
   }
 
-  const proxiedResp = await fetch(originalURL);
+  const proxiedResp = await fetch(originalURL, {
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36',
+    },
+  });
 
   if (!proxiedResp.ok) {
     return new Response(proxiedResp.body, {
