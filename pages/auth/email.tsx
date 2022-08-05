fe6d508466da8425fab657d850799790558cb8fe
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-08-05 21:46:13 +0800

    fix: why did I import all this useless stuff
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- pages/auth/email.tsx
+++ pages/auth/email.tsx
@@ -1,14 +1,5 @@
 import type { NextPage } from 'next';
 
-import { useEffect, useState } from 'react';
-
-import { supabaseClient } from '@supabase/auth-helpers-nextjs';
-import { getUserLists } from '~/lib/supabase';
-import { loaderFormat } from '~/lib/strings';
-
-import { useUserMore } from '~/hooks/useUserMore';
-import { useRequireAuth } from '~/hooks/useRequireAuth';
-
 import GlobalLayout from '~/components/GlobalLayout';
 
 const Dashboard: NextPage = () => {
