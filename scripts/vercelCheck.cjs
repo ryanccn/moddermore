89ce7ac5f851f06797a2985f14c12dd473e541e5
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-08-07 11:27:50 +0800

    chore(ci): add vercel ignored build step [skip ci]
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

---
+++ scripts/vercelCheck.cjs
@@ -0,0 +1,22 @@
+const shouldDeploy = () => {
+  console.log('Should deploy!');
+  process.exit(0);
+};
+
+const shouldntDeploy = () => {
+  console.log("Shouldn't deploy!");
+  process.exit(1);
+};
+
+if (process.env.VERCEL_ENV !== 'production') {
+  shouldntDeploy();
+}
+
+if (
+  process.env.VERCEL_GIT_COMMIT_MESSAGE &&
+  process.env.VERCEL_GIT_COMMIT_MESSAGE.includes('[skip ci]')
+) {
+  shouldntDeploy();
+}
+
+shouldDeploy();
