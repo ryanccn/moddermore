d265f81c330f61b0f03b5bd556707886b3f38a2f
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-08-07 11:08:06 +0800

    feat: slight landing page tweaks
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- components/GlobalLayout.tsx
+++ components/GlobalLayout.tsx
@@ -1,18 +1,14 @@
-import { supabaseClient } from '@supabase/auth-helpers-nextjs';
-import { useUser } from '@supabase/auth-helpers-react';
 import Head from 'next/head';
-import Link from 'next/link';
-
+import Navbar from './Navbar';
 import Footer from './Footer';
-import { PlusIcon, UserIcon, ExternalLinkIcon } from '@heroicons/react/outline';
 
-import { ReactNode, useCallback } from 'react';
-import { useRouter } from 'next/router';
+import type { ReactNode } from 'react';
 
 interface Props {
   title: string;
   titleSuffix?: boolean;
   displayTitle?: boolean | string;
+  isLandingPage?: boolean;
   children: ReactNode | ReactNode[];
 }
 
@@ -20,62 +16,18 @@ export default function GlobalLayout({
   title,
   titleSuffix = true,
   displayTitle = true,
+  isLandingPage = false,
   children,
 }: Props) {
-  const { user, isLoading } = useUser();
-  const router = useRouter();
-
-  const signOut = useCallback(() => {
-    supabaseClient.auth.signOut();
-  }, []);
-
   return (
     <>
       <Head>
         <title>{titleSuffix ? `${title} / Moddermore` : title}</title>
       </Head>
 
-      <nav className="mb-28 flex items-center justify-between px-6 py-4 shadow-sm">
-        <div className="flex items-center space-x-2">
-          <Link href={user ? '/dashboard' : '/'}>
-            <a className="text-2xl font-bold">Moddermore</a>
-          </Link>
-        </div>
-        <div className="flex items-center space-x-2">
-          {!isLoading ? (
-            user ? (
-              <>
-                <Link href="/new">
-                  <a className="primaryish-button">
-                    <PlusIcon className="block h-5 w-5" />
-                    <span>Create</span>
-                  </a>
-                </Link>
-                <button
-                  className="primaryish-button bg-transparent hover:bg-red-500/10 hover:text-red-400 hover:brightness-100 focus:ring-red-400/40"
-                  onClick={signOut}
-                >
-                  <ExternalLinkIcon className="block h-5 w-5" />
-                  <span>Sign out</span>
-                </button>
-              </>
-            ) : (
-              <Link href="/auth/signin">
-                <a className="primaryish-button">
-                  <UserIcon className="block h-5 w-5" />
-                  <span>Sign in / Sign up</span>
-                </a>
-              </Link>
-            )
-          ) : (
-            <div className="primaryish-button animate-pulse bg-zinc-300 px-16 dark:bg-zinc-700">
-              &nbsp;
-            </div>
-          )}
-        </div>
-      </nav>
+      {!isLandingPage ? <Navbar /> : null}
 
-      <main className="layout">
+      <main className={!isLandingPage ? 'layout mt-28' : 'px-2'}>
         {displayTitle === true ? (
           <h1 className="title">{title}</h1>
         ) : typeof displayTitle === 'string' ? (
---
+++ components/Navbar.tsx
@@ -0,0 +1,65 @@
+import { supabaseClient } from '@supabase/auth-helpers-nextjs';
+import { useUser } from '@supabase/auth-helpers-react';
+import { useCallback } from 'react';
+
+import Link from 'next/link';
+import { ExternalLinkIcon, PlusIcon, UserIcon } from '@heroicons/react/outline';
+import clsx from 'clsx';
+
+const Navbar = ({ isLandingPage = false }: { isLandingPage?: boolean }) => {
+  const { user, isLoading } = useUser();
+
+  const signOut = useCallback(() => {
+    supabaseClient.auth.signOut();
+  }, []);
+
+  return (
+    <nav
+      className={clsx([
+        'flex w-full items-center justify-between shadow-sm',
+        isLandingPage ? 'mb-24 bg-transparent py-4' : 'px-6 py-4',
+      ])}
+    >
+      <div className="flex items-center space-x-2">
+        <Link href={user ? '/dashboard' : '/'}>
+          <a className="text-2xl font-bold">Moddermore</a>
+        </Link>
+      </div>
+
+      <div className="flex items-center space-x-2">
+        {!isLoading ? (
+          user ? (
+            <>
+              <Link href="/new">
+                <a className="primaryish-button">
+                  <PlusIcon className="block h-5 w-5" />
+                  <span>Create</span>
+                </a>
+              </Link>
+              <button
+                className="primaryish-button bg-transparent hover:bg-red-500/10 hover:text-red-400 hover:brightness-100 focus:ring-red-400/40"
+                onClick={signOut}
+              >
+                <ExternalLinkIcon className="block h-5 w-5" />
+                <span>Sign out</span>
+              </button>
+            </>
+          ) : (
+            <Link href="/auth/signin">
+              <a className="primaryish-button">
+                <UserIcon className="block h-5 w-5" />
+                <span>Sign in / Sign up</span>
+              </a>
+            </Link>
+          )
+        ) : (
+          <div className="primaryish-button animate-pulse bg-zinc-300 px-16 dark:bg-zinc-700">
+            &nbsp;
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
