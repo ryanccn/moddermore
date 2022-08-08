75f9671121b468b24fbe4635d297ca9e72d372c7
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-08-08 23:01:07 +0800

    fix: false assumptions about CFCore fingerprint search
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- lib/import/parseModFolder.ts
+++ lib/import/parseModFolder.ts
@@ -51,6 +51,8 @@ const parseMod = async (file: Uint8Array): Promise<Mod | null> => {
 
   if (cfRes.ok) {
     const cfData = (await cfRes.json()) as CurseForgeSpecialtyResponse;
+
+    if (cfData.data.exactMatches.length === 0) return null;
     return {
       id: `${cfData.data.exactMatches[0].file.modId}`,
       provider: 'curseforge',
