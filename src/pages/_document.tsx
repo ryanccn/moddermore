2232e668f32268e85dfe704d0f51de8136876cfc
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-10-29 22:44:04 +0800

    chore(deps): upgrade to Next.js 13 (#41)
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.bunny.net" />
-        <link
-          href="https://fonts.bunny.net/css?family=lobster:400"
-          rel="stylesheet"
-        />
         <meta
           name="google-site-verification"
           content="gAuvmm-r_a-HDIw51Wb2tUWvBMM6MxOdpSVunn2bECY"
--- src/pages/new/index.tsx
+++ src/pages/new/index.tsx
@@ -17,29 +17,21 @@ const NewIndex: NextPage = () => {
   return (
     <GlobalLayout title="Create a new list">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <Link href="/new/ferium">
-          <a className="new-card">
-            <CommandLineIcon className="block h-10 w-10 stroke-[1.5]" />
-            <span>Ferium</span>
-          </a>
+        <Link href="/new/ferium" className="new-card">
+          <CommandLineIcon className="block h-10 w-10 stroke-[1.5]" />
+          <span>Ferium</span>
         </Link>
-        <Link href="/new/prism">
-          <a className="new-card">
-            <PrismIcon className="block h-10 w-10 stroke-[1.5]" />
-            <span>MultiMC / Prism instance</span>
-          </a>
+        <Link href="/new/prism" className="new-card">
+          <PrismIcon className="block h-10 w-10 stroke-[1.5]" />
+          <span>MultiMC / Prism instance</span>
         </Link>
-        <Link href="/new/folder">
-          <a className="new-card">
-            <FolderIcon className="block h-10 w-10 stroke-[1.5]" />
-            <span>Folder</span>
-          </a>
+        <Link href="/new/folder" className="new-card">
+          <FolderIcon className="block h-10 w-10 stroke-[1.5]" />
+          <span>Folder</span>
         </Link>
-        <Link href="/new/manual">
-          <a className="new-card">
-            <ArchiveBoxIcon className="block h-10 w-10 stroke-[1.5]" />
-            <span>Manual</span>
-          </a>
+        <Link href="/new/manual" className="new-card">
+          <ArchiveBoxIcon className="block h-10 w-10 stroke-[1.5]" />
+          <span>Manual</span>
         </Link>
       </div>
     </GlobalLayout>
