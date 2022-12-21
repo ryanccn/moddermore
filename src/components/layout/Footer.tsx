0fcae17180f760c64622ef37421640e1126eb4e1
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-12-21 23:22:29 +0800

    fix: reorder footer
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,19 +5,19 @@ export const Footer = () => {
   return (
     <footer className="moddermore-footer">
       <a
-        href="https://github.com/ryanccn/moddermore"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Code open source under AGPLv3 license
-      </a>
-      <Link href="/legal/privacy">Privacy Policy</Link>
-      <a
         href="https://discord.gg/uf6kxSawfc"
-        className="!text-indigo-500 hover:!brightness-125 dark:!text-indigo-400"
+        className="primaryish-button mb-4 !font-semibold !text-white"
         rel="noreferrer"
       >
         Join the Discord
+      </a>
+      <Link href="/legal/privacy">Privacy Policy</Link>
+      <a
+        href="https://github.com/ryanccn/moddermore"
+        className="!mb-4"
+        rel="noreferrer"
+      >
+        Code open source under AGPLv3
       </a>
       <PoweredByVercel />
     </footer>
