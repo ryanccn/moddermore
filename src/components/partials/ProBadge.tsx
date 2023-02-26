1029cbaa4c0ce90a652415f246a855be08d0cd7a
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-02-26 22:31:47 +0800

    feat: add checkmark in badge for Pro subscribers
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- src/components/partials/ProBadge.tsx
+++ src/components/partials/ProBadge.tsx
@@ -1,16 +1,23 @@
+import { CheckIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 
 export const ProBadge = ({ className }: { className?: string }) => {
+  const session = useSession();
+
   return (
     <Link
       className={clsx(
-        'inline-block w-auto self-start rounded-full bg-gradient-to-br from-indigo-500 to-green-500 px-2 py-0.5 text-xs font-bold uppercase text-white',
+        'flex w-auto flex-row items-center gap-x-1 self-start rounded-full bg-gradient-to-br from-indigo-500 to-green-500 px-2 py-0.5 text-xs font-bold uppercase text-white',
         className
       )}
       href="/pro"
     >
-      Pro
+      {session?.data?.extraProfile.plan === 'pro' && (
+        <CheckIcon className="block h-3 w-3" strokeWidth={3} />
+      )}
+      <span>Pro</span>
     </Link>
   );
 };
