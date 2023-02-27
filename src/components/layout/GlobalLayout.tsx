c271385e4dc906b77cdb04433fc51ddcd2927342
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-02-27 23:25:35 +0800

    experimental: dashboard-like layout
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- src/components/layout/GlobalLayout.tsx
+++ src/components/layout/GlobalLayout.tsx
@@ -62,7 +62,7 @@ export const GlobalLayout = ({
         <meta name="twitter:image" content="https://moddermore.net/e.png" />
       </Head>
 
-      <Navbar isLandingPage={isLandingPage} />
+      <Navbar />
 
       <main
         className={
