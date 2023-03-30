cd9bf9a75a39b0c11e271836401de33aae541180
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-03-30 11:23:35 +0800

    fix: rich mod spacing issues on mobile
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- src/components/partials/RichModDisplay.tsx
+++ src/components/partials/RichModDisplay.tsx
@@ -38,75 +38,73 @@ export const RichModDisplay = ({
         className
       )}
     >
-      <div className="flex w-full">
-        <div className="shrink-0">
-          {data.iconUrl && (
-            <img
-              src={data.iconUrl}
-              alt={`Icon of ${data.name}`}
-              className="mr-4 h-16 w-16 rounded-2xl object-contain"
-              width={64}
-              height={64}
-            />
-          )}
-        </div>
+      <div className="shrink-0">
+        {data.iconUrl && (
+          <img
+            src={data.iconUrl}
+            alt={`Icon of ${data.name}`}
+            className="mr-4 h-16 w-16 rounded-2xl object-contain"
+            width={64}
+            height={64}
+          />
+        )}
+      </div>
 
-        <div className="flex grow flex-col gap-x-2 sm:flex-row sm:justify-between">
-          <div className="flex flex-col justify-between gap-y-2">
-            <div className="flex flex-col gap-y-1">
-              <h2 className="mr-2 text-xl font-bold">{data.name}</h2>
-              <p className="my-0.5">{data.description}</p>
-            </div>
-
-            {incompatible && (
-              <div className="font-medium text-red-500 dark:text-red-400">
-                Incompatible with current list!
-              </div>
-            )}
-
-            <a
-              className="group flex flex-wrap items-center gap-x-1 self-start underline decoration-black/50 underline-offset-2 transition-all hover:decoration-black/75 dark:decoration-white/50 dark:hover:decoration-white/75"
-              href={data.href}
-              rel="noreferrer noopener"
-            >
-              {providerFormat(data.provider)}
-              <ArrowUpRightIcon className="block h-4 w-4" />
-            </a>
+      <div className="flex grow flex-col gap-y-2 gap-x-2 sm:flex-row sm:justify-between">
+        <div className="flex flex-col justify-between gap-y-2">
+          <div className="flex flex-col gap-y-1">
+            <h2 className="mr-2 text-xl font-bold">{data.name}</h2>
+            <p className="my-0.5">{data.description}</p>
           </div>
 
-          <div className="min-w-fit">
-            {data.downloads && (
-              <div className="mb-2 flex items-center sm:justify-end">
-                <ArrowDownTrayIcon className="mr-1 h-4 w-4" />
-                <p className="font-medium">
-                  <strong>{numberFormat(data.downloads)}</strong> downloads
-                </p>
-              </div>
-            )}
-            <div className="flex flex-col sm:items-end">
-              {onClick && (
-                <>
-                  {buttonType === 'delete' && (
-                    <button
-                      className="primaryish-button oh-no mb-2"
-                      onClick={onClick}
-                    >
-                      <TrashIcon className="block h-5 w-5" />
-                      <span>Delete</span>
-                    </button>
-                  )}
-                  {buttonType === 'add' && (
-                    <button
-                      className="primaryish-button mb-2 bg-green-500"
-                      onClick={onClick}
-                    >
-                      <PlusIcon className="block h-5 w-5" />
-                      <span>Add</span>
-                    </button>
-                  )}
-                </>
-              )}
+          {incompatible && (
+            <div className="font-medium text-red-500 dark:text-red-400">
+              Incompatible with current list!
             </div>
+          )}
+
+          <a
+            className="group flex flex-wrap items-center gap-x-1 self-start underline decoration-black/50 underline-offset-2 transition-all hover:decoration-black/75 dark:decoration-white/50 dark:hover:decoration-white/75"
+            href={data.href}
+            rel="noreferrer noopener"
+          >
+            {providerFormat(data.provider)}
+            <ArrowUpRightIcon className="block h-4 w-4" />
+          </a>
+        </div>
+
+        <div className="min-w-fit">
+          {data.downloads && (
+            <div className="mb-2 flex items-center sm:justify-end">
+              <ArrowDownTrayIcon className="mr-1 h-4 w-4" />
+              <p className="font-medium">
+                <strong>{numberFormat(data.downloads)}</strong> downloads
+              </p>
+            </div>
+          )}
+          <div className="flex flex-col sm:items-end">
+            {onClick && (
+              <>
+                {buttonType === 'delete' && (
+                  <button
+                    className="primaryish-button oh-no mb-2"
+                    onClick={onClick}
+                  >
+                    <TrashIcon className="block h-5 w-5" />
+                    <span>Delete</span>
+                  </button>
+                )}
+                {buttonType === 'add' && (
+                  <button
+                    className="primaryish-button mb-2 bg-green-500"
+                    onClick={onClick}
+                  >
+                    <PlusIcon className="block h-5 w-5" />
+                    <span>Add</span>
+                  </button>
+                )}
+              </>
+            )}
           </div>
         </div>
       </div>
