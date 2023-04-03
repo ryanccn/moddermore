0a00d5cd1da0800265961a321a80c99b15af815a
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-04-03 21:57:14 +0800

    chore: remove downtime message
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- src/components/layout/GlobalLayout.tsx
+++ src/components/layout/GlobalLayout.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
 
-import { useEffect, useMemo, useState, type ReactNode } from 'react';
+import { useMemo, type ReactNode } from 'react';
 
 interface Props {
   title: string;
@@ -29,23 +29,6 @@ export const GlobalLayout = ({
     () => (titleSuffix ? `${title} / Moddermore` : title),
     [title, titleSuffix]
   );
-
-  const [showDowntimeMessage, setShowDowntimeMessage] = useState(false);
-
-  useEffect(() => {
-    if (!localStorage.getItem('downtimeMessageViewed-2-26')) {
-      setShowDowntimeMessage(true);
-      localStorage.setItem('downtimeMessageViewed-2-26', 'true');
-
-      const timeout = setTimeout(() => {
-        setShowDowntimeMessage(false);
-      }, 4000);
-
-      return () => {
-        clearTimeout(timeout);
-      };
-    }
-  }, []);
 
   return (
     <>
@@ -99,15 +82,6 @@ export const GlobalLayout = ({
         ) : null}
         {children}
       </main>
-
-      {showDowntimeMessage && (
-        <div className="fixed bottom-0 right-0 m-6 flex max-w-md rounded-lg bg-red-100 p-6 text-sm font-medium dark:bg-red-900">
-          Our main database has gone down and there has been significant data
-          loss. We are actively trying to recover this data. Meanwhile, we have
-          moved to a new database cluster so you can continue using this
-          service.
-        </div>
-      )}
 
       <Footer isLandingPage={isLandingPage} />
     </>
