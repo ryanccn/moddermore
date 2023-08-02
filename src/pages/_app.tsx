a7951c28523e44b67c4d87cb24365415066440a5
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-08-02 21:14:19 +0800

    feat: add Google AdSense script
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ import {
   ExclamationCircleIcon,
 } from '@heroicons/react/20/solid';
 import { Session } from 'next-auth';
+import Script from 'next/script';
 
 function MyApp({
   Component,
@@ -25,7 +26,7 @@ function MyApp({
         process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 8) ?? 'unknown'
       }`,
       'padding: 0.5rem 0.375rem 0.375rem 0.5rem; font-weight: bold; border-radius: 0.25rem 0 0 0.25rem; background-color: #6366F1; border: 1px solid transparent;',
-      'padding: 0.5rem 0.375rem 0.375rem 0.5rem; font-weight: medium; border-radius: 0 0.25rem 0.25rem 0; background-color: transparent; border: 1px solid #6366F1;'
+      'padding: 0.5rem 0.375rem 0.375rem 0.5rem; font-weight: medium; border-radius: 0 0.25rem 0.25rem 0; background-color: transparent; border: 1px solid #6366F1;',
     );
   }, []);
 
@@ -33,6 +34,10 @@ function MyApp({
     <PlausibleProvider domain="moddermore.net">
       <SessionProvider session={session}>
         <NextNProgress color="#6366F1" />
+        <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2302346218691689"
+          crossOrigin="anonymous"
+        />
         <Component {...pageProps} />
         <Toaster
           position="bottom-right"
