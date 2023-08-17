a5da8325b2d4e96ca3f0a55df8bcf643a7076c6a
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-08-17 20:56:58 +0800

    fix: resolve forge loader by game version
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- src/lib/export/loaderVersions.ts
+++ src/lib/export/loaderVersions.ts
@@ -1,27 +1,56 @@
 import { XMLParser } from 'fast-xml-parser';
 
-const getFromMaven = async (url: string): Promise<string> => {
+type MavenMetadata = {
+  metadata: {
+    groupId: string;
+    artifactId: string;
+    versioning: {
+      release: string;
+      latest: string;
+      lastUpdated: string;
+      versions: { version: string }[];
+    };
+  };
+};
+
+const getFromMaven = async ({
+  url,
+  gameVersion,
+}: {
+  url: string;
+  gameVersion?: string;
+}): Promise<string> => {
   const parser = new XMLParser();
   const mavenTextResponse = await fetch(url).then((r) => {
     if (!r.ok) throw new Error(`${r.url} ${r.status} ${r.statusText}`);
     return r.text();
   });
 
-  const mavenXML = parser.parse(mavenTextResponse);
+  const mavenXML = parser.parse(mavenTextResponse) as MavenMetadata;
+
+  if (gameVersion) {
+    const matchingVersion = mavenXML.metadata.versioning.versions.find((v) =>
+      v.version.startsWith(gameVersion),
+    );
+
+    if (matchingVersion) return matchingVersion.version;
+  }
+
   return mavenXML.metadata.versioning.latest;
 };
 
 export const getLatestFabric = () =>
-  getFromMaven(
-    'https://maven.fabricmc.net/net/fabricmc/fabric-loader/maven-metadata.xml'
-  );
+  getFromMaven({
+    url: 'https://maven.fabricmc.net/net/fabricmc/fabric-loader/maven-metadata.xml',
+  });
 
 export const getLatestQuilt = () =>
-  getFromMaven(
-    'https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-loader/maven-metadata.xml'
-  );
+  getFromMaven({
+    url: 'https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-loader/maven-metadata.xml',
+  });
 
-export const getLatestForge = () =>
-  getFromMaven(
-    'https://maven.minecraftforge.net/net/minecraftforge/forge/maven-metadata.xml'
-  );
+export const getLatestForge = (gameVersion: string) =>
+  getFromMaven({
+    url: 'https://maven.minecraftforge.net/net/minecraftforge/forge/maven-metadata.xml',
+    gameVersion,
+  });
