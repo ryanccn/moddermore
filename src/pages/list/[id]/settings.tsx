123e14d7abfdc86d154d317c6ac4b42fc867fdca
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-11-08 21:33:46 +0800

    feat: add beautiful visibility input
    Signed-off-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- src/pages/list/[id]/settings.tsx
+++ src/pages/list/[id]/settings.tsx
@@ -1,13 +1,13 @@
 import { type GetServerSideProps, type NextPage } from "next";
 
 import { useRouter } from "next/router";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { ReactNode, useCallback, useEffect, useMemo, useState } from "react";
 
 import { getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
 import { authOptions } from "~/lib/authOptions";
 
-import { ArrowLeftIcon, SaveIcon } from "lucide-react";
+import { ArrowLeftIcon, GlobeIcon, LockIcon, SaveIcon, ShieldIcon } from "lucide-react";
 import Link from "next/link";
 import { GlobalLayout } from "~/components/layout/GlobalLayout";
 import { Spinner } from "~/components/partials/Spinner";
@@ -18,10 +18,49 @@ import { getSpecificList } from "~/lib/db";
 import minecraftVersions from "~/lib/minecraftVersions.json";
 
 import type { ModList, ModLoader } from "~/types/moddermore";
+import { twMerge } from "tailwind-merge";
 
 interface PageProps {
   data: ModList;
 }
+
+const MMRadio = <T extends string>({
+  name,
+  value,
+  currentValue,
+  onCheck,
+  children,
+  className,
+}: {
+  name: string;
+  value: T;
+  currentValue: T;
+  onCheck?: (v: T) => void | Promise<void>;
+  className?: string;
+  children?: ReactNode | ReactNode[];
+}) => {
+  return (
+    <label
+      className={twMerge(
+        "flex w-full flex-col items-start gap-y-2 rounded-md p-4 hover:cursor-pointer",
+        value === currentValue ? "bg-indigo-500 text-white" : "bg-neutral-100 dark:bg-neutral-800",
+        className,
+      )}
+    >
+      <input
+        type="radio"
+        className="sr-only"
+        name={name}
+        value={value}
+        checked={value === currentValue}
+        onChange={(ev) => {
+          if (onCheck && ev.target.value) onCheck(ev.target.value as T);
+        }}
+      />
+      {children}
+    </label>
+  );
+};
 
 const ListSettings: NextPage<PageProps> = ({ data }) => {
   const session = useSession({ required: true });
@@ -89,6 +128,7 @@ const ListSettings: NextPage<PageProps> = ({ data }) => {
         <ArrowLeftIcon className="block h-3 w-3" />
         <span className="text-sm font-medium text-neutral-700 dark:text-neutral-300">Back</span>
       </Link>
+
       <form
         className="mb-16 flex flex-col gap-y-4"
         onSubmit={(e) => {
@@ -122,58 +162,76 @@ const ListSettings: NextPage<PageProps> = ({ data }) => {
           />
         </label>
 
-        <label className="moddermore-form-label">
-          <span>Visibility</span>
-          <select
-            name="visibility"
-            value={visibility}
-            aria-label="Visibility"
-            required
-            onChange={(e) => {
-              setVisibility(e.target.value as typeof visibility);
-            }}
-          >
-            <option value="private">Private</option>
-            <option value="unlisted">Unlisted</option>
-            <option value="public">Public</option>
-          </select>
-        </label>
+        <div className="flex flex-col gap-y-2">
+          <span className="font-display text-sm font-bold">Visibility</span>
+          <div className="flex w-full flex-col gap-2 self-stretch md:flex-row">
+            <MMRadio name="visibility" value={"private"} currentValue={visibility} onCheck={setVisibility}>
+              <div className="flex flex-row items-center gap-x-1.5">
+                <LockIcon className="block h-4 w-4 shrink-0 opacity-75" strokeWidth={2.5} />
+                <span className="font-display text-lg font-medium">Private</span>
+              </div>
+              <span className="text-xs opacity-60">
+                Only accessible by you. Others visiting the link will see a 404.
+              </span>
+            </MMRadio>
+            <MMRadio name="visibility" value={"unlisted"} currentValue={visibility} onCheck={setVisibility}>
+              <div className="flex flex-row items-center gap-x-1.5">
+                <ShieldIcon className="block h-4 w-4 shrink-0 opacity-75" strokeWidth={2.5} />
+                <span className="font-display text-lg font-medium">Unlisted</span>
+              </div>
+              <span className="text-xs opacity-60">
+                Anyone with the link will be able to see the list, and it might be indexed by search engines.
+              </span>
+            </MMRadio>
+            <MMRadio name="visibility" value={"public"} currentValue={visibility} onCheck={setVisibility}>
+              <div className="flex flex-row items-center gap-x-1.5">
+                <GlobeIcon className="block h-4 w-4 shrink-0 opacity-75" strokeWidth={2.5} />
+                <span className="font-display text-lg font-medium">Public</span>
+              </div>
+              <span className="text-xs opacity-60">
+                Anyone with the link will be able to see the list, and it will be available in Search.
+              </span>
+            </MMRadio>
+          </div>
+        </div>
 
-        <label className="moddermore-form-label">
-          <span>Game version</span>
-          <select
-            name="game-version"
-            value={gameVersion}
-            aria-label="Game version"
-            required
-            onChange={(e) => {
-              setGameVersion(e.target.value);
-            }}
-          >
-            {minecraftVersions.map((v) => (
-              <option value={v} key={v}>
-                {v}
-              </option>
-            ))}
-          </select>
-        </label>
+        <div className="flex flex-col gap-4 md:flex-row">
+          <label className="moddermore-form-label md:w-1/2">
+            <span>Game version</span>
+            <select
+              name="game-version"
+              value={gameVersion}
+              aria-label="Game version"
+              required
+              onChange={(e) => {
+                setGameVersion(e.target.value);
+              }}
+            >
+              {minecraftVersions.map((v) => (
+                <option value={v} key={v}>
+                  {v}
+                </option>
+              ))}
+            </select>
+          </label>
 
-        <label className="moddermore-form-label">
-          <span>Mod loader</span>
-          <select
-            name="modloader"
-            value={modLoader}
-            aria-label="Mod loader"
-            required
-            onChange={(e) => {
-              setModLoader(e.target.value as ModLoader);
-            }}
-          >
-            <option value="quilt">Quilt</option>
-            <option value="fabric">Fabric</option>
-            <option value="forge">Forge</option>
-          </select>
-        </label>
+          <label className="moddermore-form-label md:w-1/2">
+            <span>Mod loader</span>
+            <select
+              name="modloader"
+              value={modLoader}
+              aria-label="Mod loader"
+              required
+              onChange={(e) => {
+                setModLoader(e.target.value as ModLoader);
+              }}
+            >
+              <option value="quilt">Quilt</option>
+              <option value="fabric">Fabric</option>
+              <option value="forge">Forge</option>
+            </select>
+          </label>
+        </div>
 
         <Button type="submit" className="mt-4 self-start" disabled={inProgress}>
           {inProgress ? <Spinner className="h-5 w-5" /> : <SaveIcon className="block h-5 w-5" />}
