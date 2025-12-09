628d66ced9944bb3f3bcc0804bc308afd1687735
Ryan Cao <hello@ryanccn.dev>
2025-12-10 07:54:59 +0800

    chore: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,14 @@ import { config } from "@ryanccn/eslint-config";
 
 export default config({
   globals: ["es2024", "node"],
-  // TODO: figure out why these do not work anymore
-  reactHooks: false,
-  next: false,
+  reactHooks: true,
+  next: true,
   rules: {
     "unicorn/import-style": "off",
     "unicorn/prefer-global-this": "off",
     "unicorn/require-module-specifiers": "off",
+    "@next/next/no-img-element": "off",
+    "react-hooks/set-state-in-effect": "off",
+    "react-hooks/immutability": "off",
   },
 });
